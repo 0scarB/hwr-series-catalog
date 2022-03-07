17a70dc2ca32df312dd5aeefd4a9686dcd7c731b
0scarB <oscarb@protonmail.com>
2022-03-07 16:39:12 +0100

    Add store

---
+++ store.js
@@ -0,0 +1,14 @@
+const publicStore = {
+    comments: [
+        // {path: "arcane", userId: "", content: ""}
+    ]
+}
+
+const userStore = new Map();
+
+// userStore.put("<userId>", {
+//     favorites: [path1, path2],
+//     mostVisited: [
+//         {path: "", click: 0}
+//     ]
+// })
